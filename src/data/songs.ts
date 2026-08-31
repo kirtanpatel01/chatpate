b8cd5da9485edbe31862789426043a6d432f3471
Kirtan Patel <kjpatel200022@gmail.com>
2026-08-31 14:17:59 +0530

    improved ui/ux and added songs which aren't there

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -9,113 +9,113 @@ export const PLAYLIST_ID = 'PLUKBMAqy5FT0';
 export const SONGS: Song[] = [
   {
     youtubeId: "5RT6QMKJTjQ",
-    title: "Ishq Sufiyana Lyrical",
-    artist: "T-Series"
+    title: "Ishq Sufiyana",
+    artist: "Kamal Khan"
   },
   {
     youtubeId: "LN07AbMX_HU",
-    title: "Befikra FULL SONG with Lyrics",
-    artist: "T-Series"
+    title: "Befikra",
+    artist: "Meet Bros & Aditi Singh Sharma"
   },
   {
     youtubeId: "4IdgrshIpU4",
-    title: "Rishte Naate - HD VIDEO",
-    artist: "Ishtar"
+    title: "Rishte Naate",
+    artist: "Rahat Fateh Ali Khan & Tulsi Kumar"
   },
   {
     youtubeId: "uLTfIm5aptQ",
-    title: "SANAM - Sanam Mennu -",
-    artist: "Sanam"
+    title: "Sanam Mennu",
+    artist: "Sanam Puri"
   },
   {
     youtubeId: "mKnFLXJwmu4",
     title: "Taarif Karoon Kya Uski",
-    artist: "Saregama"
+    artist: "Sanam Puri"
   },
   {
     youtubeId: "4KFVySixs_E",
-    title: "Heropanti : Raat Bhar",
-    artist: "T-Series"
+    title: "Raat Bhar",
+    artist: "Arijit Singh & Shreya Ghoshal"
   },
   {
     youtubeId: "9UDkYz64ehA",
-    title: "\"Ambarsariya Fukrey\" Song By Sona Mohapatra",
-    artist: "T-Series"
+    title: "Ambarsariya",
+    artist: "Sona Mohapatra"
   },
   {
     youtubeId: "l1YtQ2YOZDg",
-    title: "Darshan Raval - Dil Mera Blast",
-    artist: "Indie Music Label"
+    title: "Dil Mera Blast",
+    artist: "Darshan Raval"
   },
   {
     youtubeId: "Gf7Lf4kiZ-A",
-    title: "SANAM - Badan Pe Sitare",
-    artist: "Sanam"
+    title: "Badan Pe Sitare",
+    artist: "Sanam Puri"
   },
   {
     youtubeId: "zwsc1rVdPxA",
-    title: "SANAM - Aajkal Tere Mere Pyar Ke Charche - Ft. Sanah Moidutty",
-    artist: "Sanam"
+    title: "Aajkal Tere Mere Pyar Ke Charche",
+    artist: "Sanam Puri & Sanah Moidutty"
   },
   {
     youtubeId: "udgrClXV26Y",
     title: "Dil Ka Jo Haal Hai",
-    artist: "T-Series"
+    artist: "Abhijeet Bhattacharya & Shreya Ghoshal"
   },
   {
     youtubeId: "1gNg337fb5E",
-    title: "Caller Tune 8K Video",
-    artist: "Zee Music Company"
+    title: "Caller Tune",
+    artist: "Neeraj Shridhar & Benny Dayal"
   },
   {
     youtubeId: "yHWPO9DDnsk",
-    title: "Bheegi Si Bhaagi Si -",
-    artist: "Sony Music India"
+    title: "Bheegi Si Bhaagi Si",
+    artist: "Mohit Chauhan & Antara Mitra"
   },
   {
     youtubeId: "eM8Mjuq4MwQ",
-    title: "Aankhein Khuli Song",
-    artist: "YRF"
+    title: "Aankhein Khuli",
+    artist: "Lata Mangeshkar & Udit Narayan"
   },
   {
     youtubeId: "ln8KreDppvI",
     title: "Besharmi Ki Height",
-    artist: "T-Series"
+    artist: "Benny Dayal & Shalmali Kholgade"
   },
   {
     youtubeId: "A5YDZGWxWhU",
-    title: "Lyrical : \"Subha Hone Na De\" Song",
-    artist: "T-Series"
+    title: "Subha Hone Na De",
+    artist: "Mika Singh & Shefali Alvares"
   },
   {
     youtubeId: "K7aKsjuwoo8",
-    title: "Tumhi Ho Bandhu - Lyrical Video",
-    artist: "Sony Music India"
+    title: "Tumhi Ho Bandhu",
+    artist: "Neeraj Shridhar & Kavita Seth"
   },
   {
     youtubeId: "qTsAdjULqwg",
     title: "Uff Teri Adaa",
-    artist: "T-Series"
+    artist: "Shankar Mahadevan & Alyssa Mendonsa"
   },
   {
     youtubeId: "Wd2B8OAotU8",
-    title: "Nashe Si Chadh Gayi Song",
-    artist: "YRF"
+    title: "Nashe Si Chadh Gayi",
+    artist: "Arijit Singh & Caralisa Monteiro"
   },
   {
     youtubeId: "BzcKINXf-rs",
     title: "Chogada",
-    artist: "T-Series"
+    artist: "Darshan Raval & Asees Kaur"
   },
   {
     youtubeId: "nJZcbidTutE",
-    title: "'Sooraj Dooba Hain'",
-    artist: "T-Series"
+    title: "Sooraj Dooba Hain",
+    artist: "Arijit Singh & Aditi Singh Sharma"
   },
   {
     youtubeId: "HqUeSjsYLNU",
-    title: "Makhna - Drive",
-    artist: "Zee Music Company"
+    title: "Makhna",
+    artist: "Tanishk Bagchi, Yasser Desai & Asees Kaur"
   },
   {
     youtubeId: "-YlmnPh-6rE",
@@ -124,317 +124,317 @@ export const SONGS: Song[] = [
   },
   {
     youtubeId: "x_elT6zkqN0",
-    title: "\"Oh Oh Jane Jaana\" Salman Khan Full Song",
-    artist: "T-Series"
+    title: "Oh Oh Jane Jaana",
+    artist: "Kamaal Khan"
   },
   {
     youtubeId: "i0_m90T04uw",
-    title: "Kamariya -",
-    artist: "Sony Music India"
+    title: "Kamariya",
+    artist: "Darshan Raval"
   },
   {
     youtubeId: "zpsVpnvFfZQ",
-    title: "'Chittiyaan Kalaiyaan'",
-    artist: "T-Series"
+    title: "Chittiyaan Kalaiyaan",
+    artist: "Kanika Kapoor & Meet Bros"
   },
   {
     youtubeId: "oAVhUAaVCVQ",
-    title: "Lyrical: Chammak Challo",
-    artist: "T-Series"
+    title: "Chammak Challo",
+    artist: "Akon & Hamsika Iyer"
   },
   {
     youtubeId: "slM5s55Jz0k",
-    title: "Thodi Si Daaru (",
-    artist: "APDHILLON"
+    title: "Thodi Si Daaru",
+    artist: "AP Dhillon"
   },
   {
     youtubeId: "_ZQ9e3NFENQ",
-    title: "OK Jaanu - Full Song Lyric Video",
-    artist: "Sony Music India"
+    title: "OK Jaanu Title Track",
+    artist: "A.R. Rahman & Srinidhi Venkatesh"
   },
   {
     youtubeId: "pElk1ShPrcE",
-    title: "Ainvayi Ainvayi Song",
-    artist: "YRF"
+    title: "Ainvayi Ainvayi",
+    artist: "Salim Merchant & Sunidhi Chauhan"
   },
   {
     youtubeId: "G6RR2_NQ530",
-    title: "Locha E Ulfat",
-    artist: "T-Series"
+    title: "Locha-E-Ulfat",
+    artist: "Benny Dayal"
   },
   {
     youtubeId: "bUTdUaVmj8o",
-    title: "Gulaabo -",
-    artist: "Zee Music Company"
+    title: "Gulaabo",
+    artist: "Vishal Dadlani & Anusha Mani"
   },
   {
     youtubeId: "LEYXdZ_rVbo",
-    title: "Thug Le Song",
-    artist: "YRF"
+    title: "Thug Le",
+    artist: "Vishal Dadlani & Shweta Pandit"
   },
   {
     youtubeId: "Ee_9J9lGgS0",
-    title: "Main Tera Hero",
-    artist: "T-Series"
+    title: "Main Tera Hero (Shanivaar Raati)",
+    artist: "Arijit Singh & Shalmali Kholgade"
   },
   {
     youtubeId: "IFYJNLZT_B0",
-    title: "Besharmi Ki Height",
-    artist: "T-Series"
+    title: "Besharmi Ki Height (Remix)",
+    artist: "Benny Dayal & Shalmali Kholgade"
   },
   {
     youtubeId: "N_KpjLhJa1k",
-    title: "HIGH HEELS TE NACHCHE",
-    artist: "T-Series"
+    title: "High Heels Te Nachche",
+    artist: "Jaz Dhami, Yo Yo Honey Singh & Aditi Singh Sharma"
   },
   {
     youtubeId: "UVbcaG2tfSE",
-    title: "Chalti Hai Kya 9 Se 12 Full Song",
-    artist: "T-Series"
+    title: "Chalti Hai Kya 9 Se 12",
+    artist: "Dev Negi & Neha Kakkar"
   },
   {
     youtubeId: "l75z7FrYRXI",
-    title: "Full Song: Muqabla",
-    artist: "T-Series"
+    title: "Muqabla",
+    artist: "Yash Narvekar & Parampara Tandon"
   },
   {
     youtubeId: "rxMmistOjCA",
     title: "Nachange Saari Raat",
-    artist: "T-Series"
+    artist: "Neeraj Shridhar & Tulsi Kumar"
   },
   {
     youtubeId: "mcL6ZErM49Q",
-    title: "The Disco Song Full Song - SOTY",
-    artist: "Sony Music India"
+    title: "The Disco Song",
+    artist: "Benny Dayal, Sunidhi Chauhan & Vishal Dadlani"
   },
   {
     youtubeId: "-ASKgzrmDtc",
-    title: "\"Dilli waali Girlfriend\" Yeh Jawaani Hai Deewani",
-    artist: "T-Series"
+    title: "Dilliwaali Girlfriend",
+    artist: "Arijit Singh & Sunidhi Chauhan"
   },
   {
     youtubeId: "oGneAab3e88",
     title: "Jai Jai Shivshankar",
-    artist: "YRF"
+    artist: "Vishal Dadlani & Benny Dayal"
   },
   {
     youtubeId: "KxCjVIFxZNo",
-    title: "Lat Lag Gayee Lyrical - Race 2",
-    artist: "Tips"
+    title: "Lat Lag Gayee",
+    artist: "Benny Dayal & Shalmali Kholgade"
   },
   {
     youtubeId: "PIyf0hMc498",
     title: "Sau Tarah Ke",
-    artist: "T-Series"
+    artist: "Jonita Gandhi & Amit Mishra"
   },
   {
     youtubeId: "HgIW7P4dsXU",
-    title: "Nachde Ne Saare -",
-    artist: "Zee Music Company"
+    title: "Nachde Ne Saare",
+    artist: "Jasleen Royal, Harshdeep Kaur & Siddharth Mahadevan"
   },
   {
     youtubeId: "udra3Mfw2oo",
-    title: "Queen: London Thumakda",
-    artist: "T-Series"
+    title: "London Thumakda",
+    artist: "Labh Janjua, Sonu Kakkar & Neha Kakkar"
   },
   {
     youtubeId: "sB-S5LBU1eA",
-    title: "Aadat Se Majboor - Full Song",
-    artist: "YRF"
+    title: "Aadat Se Majboor",
+    artist: "Benny Dayal"
   },
   {
     youtubeId: "D21Di3NXcYM",
     title: "Shaam Shaandaar",
-    artist: "Zee Music Company"
+    artist: "Amit Trivedi"
   },
   {
     youtubeId: "bh2O4oyB1UM",
     title: "Bachna Ae Haseeno Title Song",
-    artist: "YRF"
+    artist: "Kishore Kumar, Vishal Dadlani & Sumeet Kumar"
   },
   {
     youtubeId: "sY6nvCkdEKI",
-    title: "Make Some Noise For The Desi Boyz -",
-    artist: "T-Series Bollywood Classics"
+    title: "Make Some Noise For The Desi Boyz",
+    artist: "KK & Bob"
   },
   {
     youtubeId: "2yeRRm9AIxI",
-    title: "Pyaar Hota Kayi Baar Hai (",
-    artist: "T-Series"
+    title: "Pyaar Hota Kayi Baar Hai",
+    artist: "Arijit Singh"
   },
   {
     youtubeId: "iVKEcznDqDg",
-    title: "ABCD Yaariyan Feat. Yo Yo Honey Singh",
-    artist: "T-Series"
+    title: "ABCD (Yaariyan)",
+    artist: "Yo Yo Honey Singh, Benny Dayal & Shefali Alvares"
   },
   {
     youtubeId: "wdxebKzUfIM",
-    title: "Bandook Meri Laila Song",
-    artist: "T-Series"
+    title: "Bandook Meri Laila",
+    artist: "Ash King, Jigar Saraiya & Raftaar"
   },
   {
     youtubeId: "AbPqLvaLN6w",
-    title: "Soni De Nakhre -",
-    artist: "T-Series Bollywood Classics"
+    title: "Soni De Nakhre",
+    artist: "Partner (Labh Janjua, Wajid & Sneha Pant)"
   },
   {
     youtubeId: "kQBtlb9gl5s",
     title: "Jashn-e-Ishqa",
-    artist: "YRF"
+    artist: "Javed Ali & Shadab Faridi"
   },
   {
     youtubeId: "BGU3FkUGH0M",
-    title: "Asalaam-e-Ishqum Song",
-    artist: "YRF"
+    title: "Asalaam-e-Ishqum",
+    artist: "Bappi Lahiri & Neha Bhasin"
   },
   {
     youtubeId: "sm2hNJ9c8M8",
-    title: "\"Allah Maaf Kare Full Song Desi Boyz\" Feat. Akshay Kumar, Chitrangada Singh",
-    artist: "T-Series"
+    title: "Allah Maaf Kare",
+    artist: "Sonu Nigam & Shilpa Rao"
   },
   {
     youtubeId: "1Na8nKEUjYI",
-    title: "Vishal & Shekhar, Shahid Mallya, Nisha Mascarenhas, Marianne D'cruz Aiman - Kukkad",
-    artist: "Sony Music India"
+    title: "Kukkad",
+    artist: "Shahid Mallya"
   },
   {
     youtubeId: "r03GO2AlNUo",
-    title: "Paisa Paisa -Official",
-    artist: "Ishtar"
+    title: "Paisa Paisa",
+    artist: "RDB & Manak-E"
   },
   {
     youtubeId: "6CVVd9RB7CI",
     title: "GF BF",
-    artist: "T-Series"
+    artist: "Gurinder Seagal & Jacqueline Fernandez"
   },
   {
     youtubeId: "tFNWHLEIiJ0",
-    title: "Haaye Oye - QARAN ft. Ash King",
-    artist: "Sony Music India"
+    title: "Haaye Oye",
+    artist: "QARAN ft. Ash King"
   },
   {
     youtubeId: "qFkNATtc3mc",
-    title: "Ghungroo Song",
-    artist: "YRF"
+    title: "Ghungroo",
+    artist: "Arijit Singh & Shilpa Rao"
   },
   {
     youtubeId: "jZyAB2KFDls",
     title: "Bang Bang Title Track",
-    artist: "Zee Music Company"
+    artist: "Benny Dayal & Neeti Mohan"
   },
   {
     youtubeId: "m_4lF320DfQ",
     title: "Right Now Now",
-    artist: "T-Series"
+    artist: "Wajid, Suzi Q & Sunidhi Chauhan"
   },
   {
     youtubeId: "PzcSrkMKDdk",
-    title: "Halkat Jawani - 8K/4K Music Video",
-    artist: "Sony Music India"
+    title: "Halkat Jawani",
+    artist: "Sunidhi Chauhan"
   },
   {
     youtubeId: "r2Gn4VjFyOA",
-    title: "I Hate Luv Storys - 8K/4K Music Video",
-    artist: "Sony Music India"
+    title: "I Hate Luv Storys",
+    artist: "Vishal Dadlani"
   },
   {
     youtubeId: "4dsFQFCvVGU",
     title: "Kajra Re",
-    artist: "YRF"
+    artist: "Alisha Chinai, Shankar Mahadevan & Javed Ali"
   },
   {
     youtubeId: "MS55Ke6AQTc",
-    title: "Laung Da Lashkara (Official full song) \"Patiala House\"",
-    artist: "T-Series"
+    title: "Laung Da Lashkara",
+    artist: "Jasbir Jassi, Hard Kaur & Mahalakshmi Iyer"
   },
   {
     youtubeId: "BkA0lq-0f14",
-    title: "DIL CHEEZ TUJHE DEDI",
-    artist: "T-Series"
+    title: "Dil Cheez Tujhe Dedi",
+    artist: "Ankit Tiwari & Arko"
   },
   {
     youtubeId: "wc8h0MfIX6w",
-    title: "Lyrical: \"Oh Girl You're Mine\"",
-    artist: "T-Series"
+    title: "Oh Girl You're Mine",
+    artist: "Tarannum Mallik & Loy Mendonsa"
   },
   {
     youtubeId: "c7ZGREbEFa0",
-    title: "Mauja Hi Mauja 8K Full Song",
-    artist: "T-Series"
+    title: "Mauja Hi Mauja",
+    artist: "Mika Singh"
   },
   {
     youtubeId: "pw59ySNWAjs",
-    title: "Ek Main Aur Ekk Tu (Title Track) lyrical Video",
-    artist: "T-Series"
+    title: "Ek Main Aur Ekk Tu",
+    artist: "Benny Dayal & Anushka Manchanda"
   },
   {
     youtubeId: "1CgdP4cjeMw",
-    title: "Khwab Dekhe Jhoote Moote - 8K",
-    artist: "Tips"
+    title: "Khwab Dekhe Jhoote Moote",
+    artist: "Monali Thakur & Neeraj Shridhar"
   },
   {
     youtubeId: "YPpOqfIQ5ME",
-    title: "Chhaliya Song",
-    artist: "YRF"
+    title: "Chhaliya",
+    artist: "Sunidhi Chauhan"
   },
   {
     youtubeId: "ldy17W1xi-c",
     title: "Haseeno Ka Deewana",
-    artist: "T-Series"
+    artist: "Raftaar & Payal Dev"
   },
   {
     youtubeId: "-UFzWENcFj8",
     title: "Tu Hi Toh Hai",
-    artist: "Dance Masti Hits"
+    artist: "Benny Dayal"
   },
   {
     youtubeId: "5Uls9v1nnss",
-    title: "Bang Bang - Tu Meri feat Hrithik Roshan & Katrina Kaif",
-    artist: "Zee Music Company"
+    title: "Tu Meri",
+    artist: "Vishal Dadlani"
   },
   {
     youtubeId: "RRfHzBdcGGc",
-    title: "PINK LIPS 8K Song",
-    artist: "T-Series"
+    title: "Pink Lips",
+    artist: "Khushboo Grewal & Meet Bros"
   },
   {
     youtubeId: "XER6tdwIx3o",
-    title: "Lyrical",
-    artist: "YRF"
+    title: "Dhoom Again",
+    artist: "Vishal Dadlani & Dominique Cerejo"
   },
   {
     youtubeId: "2qBWL5Fplu0",
-    title: "You Are My Soniya - K3G",
-    artist: "Sony Music India"
+    title: "You Are My Soniya",
+    artist: "Sonu Nigam & Alka Yagnik"
   },
   {
     youtubeId: "9vkcYxbGdTE",
-    title: "Palat Tera Hero Idhar Hai (",
-    artist: "T-Series"
+    title: "Palat - Tera Hero Idhar Hai",
+    artist: "Arijit Singh"
   },
   {
     youtubeId: "vvLBXO94EfA",
     title: "Gandi Baat",
-    artist: "Sony Music India"
+    artist: "Mika Singh & Kalpana Patowary"
   },
   {
     youtubeId: "K8KAbu45-kE",
-    title: "UFF",
-    artist: "Zee Music Company"
+    title: "Uff",
+    artist: "Benny Dayal & Harshdeep Kaur"
   },
   {
     youtubeId: "ZTmF2v59CtI",
-    title: "\"Sheila Ki Jawani\" Full Song",
-    artist: "T-Series"
+    title: "Sheila Ki Jawani",
+    artist: "Sunidhi Chauhan & Vishal Dadlani"
   },
   {
     youtubeId: "II2EO3Nw4m0",
-    title: "Badtameez Dil Full Song HD Yeh Jawaani Hai Deewani",
-    artist: "T-Series"
+    title: "Badtameez Dil",
+    artist: "Benny Dayal & Shefali Alvares"
   },
   {
     youtubeId: "6WLugDJYXmM",
     title: "Ladki Kyon",
-    artist: "YRF"
+    artist: "Shaan & Alka Yagnik"
   }
 ];
